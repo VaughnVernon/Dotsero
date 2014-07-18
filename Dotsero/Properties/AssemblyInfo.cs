3f3f2f167701677a37d9c758c20967dd876c1780
Vaughn Vernon <vvernon@shiftmethod.com>
2014-07-17 23:02:36 -0600

    Modified to build to a library .DLL.

--- Dotsero/Properties/AssemblyInfo.cs
+++ Dotsero/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Takka")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Dotsero")]
+[assembly: AssemblyDescription("Akka-like .NET Actor Model Toolkit")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Takka")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("Vaughn Vernon")]
+[assembly: AssemblyProduct("Dotsero")]
+[assembly: AssemblyCopyright("Copyright ©  2012-2014")]
+[assembly: AssemblyTrademark("Dotsero (tm)")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
